4b343a478c899ce45912aab70a68bf974687d8e4
csdroit <73106581+csdroit@users.noreply.github.com>
2020-11-30 19:29:14 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+
+echo -e " \e[41m ======== SAVED WIFI TOOL KALI LINUX ======== \e[0m"
+touch passordwifi.txt
+
+echo -e " \n\n\n\n \t Enter the name of wifi : \c"
+read wifi
+saved= ".nmconnections"
+echo -p "Are you shure $wifi is correct ."   
+read -p "press [enter] to continue"
+
+wifi+=saved
+cd /etc/NetworkManager/system-connections/
+sudo cat $wifi
+
